f3407cd5e71b708ac2736b9eb233a683c067c6d9
Kacper Czernecki <walentyczer@gmail.com>
2026-02-26 19:00:31 +0100

    Finished Chapter 15

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+
+export default async function Page() {
+  const customers = await fetchFilteredCustomers("");
+  return <CustomersTable customers={customers} />;
 }
